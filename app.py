af2aec15daedfbaa5c338ad2acce7a3d73a80837
david franco <david.d.franco28@gmail.com>
2026-03-10 23:52:05 -0700

    Form capability established

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+class CustomerForm(FlaskForm):
+    first_name = StringField('first_name', validators=[InputRequired()])
+    last_name = StringField('last_name', validators=[InputRequired()])
+    email = StringField('email', validators=[InputRequired()])
+    phone = StringField('phone', validators=[InputRequired()])
+    desc = TextAreaField('desc')
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    form = CustomerForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        phone = form.phone.data
+        desc = form.desc.data
+        return "data submitted"
+    return render_template('home.html', form=form)
 
 @app.route('/services-1')
 def services():
-    return "service"
+    return render_template('studio.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
